e91b685e54fece5471ab0e2cd9f96e142c9d271a
Tejas Thakur <36739684+tejasthakur123@users.noreply.github.com>
2018-03-09 03:33:31 -0500

    io.sql
    Checking which queries are doing high io

---
+++ io.sql
@@ -0,0 +1,24 @@
+prompt >>>> ACTIVE INSTANCE <<<<
+prompt *************************
+
+@D:\tejas\scripts_teja\dbfseqread1.sql
+
+prompt
+
+@D:\tejas\scripts_teja\logfseqread1.sql
+
+prompt
+
+prompt *********************************
+
+prompt
+prompt
+
+prompt >>>> PASSIVE INSTANCE <<<<
+prompt **************************
+
+@D:\tejas\scripts_teja\dbfseqread2.sql
+
+prompt
+
+@D:\tejas\scripts_teja\logfseqread2.sql
