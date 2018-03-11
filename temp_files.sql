e5e71c477cd9668319c6d9fdfa9cbce03fbedcfa
Tejas Thakur <36739684+tejasthakur123@users.noreply.github.com>
2018-03-11 18:28:27 -0400

    Delete temp_files.sql

--- temp_files.sql
+++
@@ -1,6 +0,0 @@
-set lines 168 pages 200
-break on report
-compute sum of "Total GB", "GB Used" on report
-col FILE_NAME for a50
-select TABLESPACE_NAME,FILE_NAME,BYTES/1024/1024/1024 "GB Used",MAXBYTES/1024/1024/1024 "Total GB",AUTOEXTENSIBLE from dba_temp_files where TABLESPACE_NAME like '%&tbs%' order by TABLESPACE_NAME,FILE_ID
-/
